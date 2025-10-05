d5aeb8a2e0c127a5a75448dec9dd4a8c9998058b
ldssborges <ldssborges912@gmail.com>
2025-10-04 22:53:19 -0300

    adjust endpoint to finish preparation

--- src/backend.tf
+++ src/backend.tf
@@ -1,6 +1,6 @@
     terraform {
       backend "s3" {
-        bucket         = "techfood-tf-bucket"
+        bucket         = "techfood-ts-bucket"
         key            = "terraform/db-terraform.tfstate"
         region         = "us-east-1"
         encrypt        = true
